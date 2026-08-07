7e80e8bc52698a59b8211e6b0a93a54c4e359e8f
amritpatel876-lang <amritpatel.876@gmail.com>
2026-08-07 20:53:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -27,3 +27,22 @@ button.addEventListener("click", () => {
     },800);
 
 });
+const weddingDate = new Date("February 15, 2027 19:00:00").getTime();
+
+setInterval(() => {
+
+const now = new Date().getTime();
+
+const distance = weddingDate - now;
+
+const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+document.getElementById("days").innerHTML = days;
+document.getElementById("hours").innerHTML = hours;
+document.getElementById("minutes").innerHTML = minutes;
+document.getElementById("seconds").innerHTML = seconds;
+
+},1000);
